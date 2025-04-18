85a4c61dc9c2ce62548878fc716420cffea54ab7
Ankush0761 <163869431+Ankush0761@users.noreply.github.com>
2025-04-18 11:25:02 +0530

    Update clients.py

--- Jisshu/bot/clients.py
+++ Jisshu/bot/clients.py
@@ -2,8 +2,8 @@ import asyncio
 import logging
 from info import *
 from pyrogram import Client
-from Jisshu.util.config_parser import TokenParser
-from . import multi_clients, work_loads, JisshuBot
+from Barun.util.config_parser import TokenParser
+from . import multi_clients, work_loads, Barun
 
 
 async def initialize_clients():
